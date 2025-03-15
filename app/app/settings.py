a7532242a8917976d93baf4c7ad4ebe0a25a15e8
AhmedouVallMohamedLemine <ahmedouvallmohamedlemine@gmail.com>
2025-03-15 16:47:56 +0000

    configuration cors

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-+%i%a*(^bvyz75d2bno700-z&$^-6-e$!d=ut&dz9h@o3fruu#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -113,6 +114,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# email 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587 
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'ahmedouvallmohamedlemine@gmail.com' 
+EMAIL_HOST_PASSWORD = 'hwil nhto dbwd myew'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
